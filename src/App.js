ce5704dd9ac0dc719c62299290f11d46f0396792
Eric Soderberg <eric.soderberg@hpe.com>
2019-08-30 15:36:50 -0700

    Lot's of updates

--- src/App.js
+++ src/App.js
@@ -1,60 +1,88 @@
-import React from 'react';
+import React from 'react'
 import {
-  Box, Button, CheckBox, DataTable, Form, FormField, Grommet, Layer, Text,
+  Box, Button, DataTable, Form, FormField, Grommet, Layer,
+  RadioButton, Select, Text, TextArea, TextInput,
   grommet,
-} from 'grommet';
-import { Configure, Unlink } from 'grommet-icons';
+} from 'grommet'
+import {
+  Add, Checkmark, Close, Down, Next, Previous, Unlink, Up,
+} from 'grommet-icons'
 
 // const url = 'https://api.spacexdata.com/v3/launches?order=desc';
-const primaryKey = 'flight_number';
+// const primaryKey = 'flight_number';
 
-const buildProps = (obj, dataProps, prefix='') => {
-  Object.keys(obj).forEach((propertyName) => {
-    const property = `${prefix}${propertyName}`
-    if (typeof obj[propertyName] === 'string') {
-      dataProps.push({
-        property,
+const datumValue = (datum, property) => {
+  const parts = property.split('.');
+  if (parts.length === 1) {
+    return datum[property];
+  }
+  if (!datum[parts[0]]) {
+    return undefined;
+  }
+  return datumValue(datum[parts[0]], parts.slice(1).join('.'));
+}
+
+const buildProps = (data, pathPrefix = []) => {
+  let result = []
+  const obj = pathPrefix.length ? datumValue(data[0], pathPrefix.join('.')) : data[0]
+  Object.keys(obj).forEach((key) => {
+    const path = [...pathPrefix, key].join('.')
+    if (typeof obj[key] === 'string') {
+      // build options
+      const options = {}
+      data.forEach(datum => {
+        const value = datumValue(datum, path)
+        options[value] = true
+      })
+      result.push({
+        property: path,
         align: 'start',
-        header: property,
+        header: path,
+        example: obj[key],
+        options: Object.keys(options).length < 10
+          ? Object.keys(options) : undefined,
       })
-    } else if (typeof obj[propertyName] === 'number') {
-      dataProps.push({
-        property,
+    } else if (typeof obj[key] === 'number') {
+      result.push({
+        property: path,
         align: 'end',
-        header: property,
+        header: path,
+        example: obj[key],
       })
-    } else if (typeof obj[propertyName] === 'boolean') {
-      dataProps.push({
-        property,
+    } else if (typeof obj[key] === 'boolean') {
+      result.push({
+        property: path,
         align: 'center',
-        header: property,
-        render: value => (
-          <Box pad="small" background={value[propertyName] ? 'brand' : 'light-4'} />
-        )
+        header: path,
+        render: value => (datumValue(value, path) ? <Checkmark /> : null),
+        options: [true, false],
       })
-    } else if (Array.isArray(obj[propertyName])) {
+    } else if (Array.isArray(obj[key])) {
       // TODO
-    } else if (obj[propertyName] && typeof obj[propertyName] === 'object') {
-      buildProps(obj[propertyName], dataProps, `${propertyName}.`)
+    } else if (obj[key] && typeof obj[key] === 'object') {
+      result = result.concat(buildProps(data, path.split('.')))
     }
   })
+  return result
 }
 
 const App = () => {
   const [apiUrl, setApiUrl] = React.useState()
+  const [primaryKey, setPrimaryKey] = React.useState()
+  const [fullData, setFullData] = React.useState([])
   const [data, setData] = React.useState([])
   const [dataProps, setDataProps] = React.useState([])
   const [config, setConfig] = React.useState([])
   const [columns, setColumns] = React.useState([])
-  const [edit, setEdit] = React.useState()
+  const [edit, setEdit] = React.useState(true)
+  const [datum, setDatum] = React.useState()
+  const [search, setSearch] = React.useState('')
 
   // load from local storage
   React.useEffect(() => {
     let stored = localStorage.getItem('config')
     if (stored) {
       setConfig(JSON.parse(stored))
-    } else {
-      setConfig([primaryKey])
     }
     stored = localStorage.getItem('apiUrl')
     if (stored) {
@@ -67,28 +95,47 @@ const App = () => {
 
   // set columns when config or dataProps change 
   React.useEffect(() => {
-    if (config.length > 0 && data.length > 0) {
+    if (config.length > 0 && dataProps.length > 0) {
       localStorage.setItem('config', JSON.stringify(config))
-      const nextColumns = config.map(name => dataProps.filter(p => p.property === name)[0])
+      const nextColumns = config
+        .map((path) => dataProps.find(p => p.property === path))
       setColumns(nextColumns)
     }
-  }, [config, data, dataProps])
+  }, [config, dataProps])
 
   // load api data
   React.useEffect(() => {
     if (apiUrl) {
       fetch(apiUrl)
       .then(response => response.json())
-      .then(nextData => {
-        const first = nextData[0];
-        const nextDataProps = [];
-        buildProps(first, nextDataProps);
-        console.log('!!!', first, nextDataProps);
+      .then(nextFullData => {
+        const nextDataProps = buildProps(nextFullData)
         setDataProps(nextDataProps)
-        setData(nextData)
+        setFullData(nextFullData)
+        setData(nextFullData)
       })
     }
   }, [apiUrl])
+
+  React.useEffect(() => {
+    const nextData = fullData.filter((datum) =>
+      // check if any property has a filter that doesn't match
+      !dataProps.some(dataProp => {
+        if (dataProp.searchText) {
+          const value = datumValue(datum, dataProp.property)
+          return !(new RegExp(dataProp.searchText, 'i').test(value))
+        }
+        if (dataProp.values && dataProp.values.length > 0) {
+          const value = datumValue(datum, dataProp.property)
+          return !dataProp.values.some(v => v === value)
+        }
+        return false
+      })
+    )
+    setData(nextData)
+  }, [fullData, dataProps])
+
+  const searchExp = search ? new RegExp(search, 'i') : undefined
 
   return (
     <Grommet full theme={grommet}>
@@ -102,41 +149,182 @@ const App = () => {
           </Form>
         </Box>
       ) : (
-        <Box>
-          <Box flex={false} direction="row" align="center" justify="between">
-            <Button icon={<Unlink />} hoverIndicator onClick={() => setApiUrl(undefined)} />
-            <Text>{apiUrl}</Text>
-            <Button icon={<Configure />} hoverIndicator onClick={() => setEdit(!edit)} />
-          </Box>
-          <DataTable
-            primaryKey={primaryKey}
-            columns={columns}
-            data={data}
-          />
-          {edit && (
-            <Layer
-              position="right"
-              onEsc={() => setEdit(false)}
-              onClickOutside={() => setEdit(false)}
+        <Box direction="row">
+          <Box flex="grow" align="center">
+            <Box
+              alignSelf="stretch"
+              flex={false}
+              direction="row"
+              align="center"
+              justify="between"
+              gap="medium"
             >
-              <Box flex={false}>
-                {dataProps.map(property => (
-                  <Box key={property.property}>
-                    <CheckBox
-                      label={property.property}
-                      checked={columns.some(c => c.property === property.property)}
-                      onChange={(event) => {
-                        if (event.target.checked) {
-                          setConfig([...config, property.property]);
-                        } else {
-                          setConfig(config.filter(c => c !== property.property));
-                        }
-                      }}
-                    />
-                  </Box>
-                ))}
+              <Button
+                icon={<Unlink />}
+                hoverIndicator
+                onClick={() => setApiUrl(undefined)}
+              />
+              <Text>{apiUrl}</Text>
+              <Button
+                icon={edit ? <Next /> : <Previous />}
+                hoverIndicator
+                onClick={() => setEdit(!edit)}
+              />
+            </Box>
+
+            <Box flex={false}>
+              <DataTable
+                columns={columns}
+                primaryKey={primaryKey}
+                data={data}
+                onClickRow={({ datum }) => setDatum(datum)}
+              />
+            </Box>
+          </Box>
+
+          {datum && (
+            <Layer
+              full
+              margin="large"
+              onEsc={() => setDatum(undefined)}
+              onClickOutside={() => setDatum(undefined)}
+            >
+              <Box flex={false} direction="row" justify="end">
+                <Button
+                  icon={<Close />}
+                  hoverIndicator
+                  onClick={() => setDatum(undefined)}
+                />
               </Box>
+              <TextArea fill value={JSON.stringify(datum, null, 4)} />
             </Layer>
+          )}
+          {edit && dataProps.length > 0 && (
+            <Box flex={false} overflow="auto" background="dark-1" pad="small">
+              <Box flex={false}>
+                <TextInput
+                  placeholder="search"
+                  value={search}
+                  onChange={(event) => setSearch(event.target.value)}
+                />
+                {config
+                // omit object properties which aren't columns
+                .filter(path =>
+                  !dataProps.some(p => p.property === path && p.dataProps))
+                .filter(path => !search || searchExp.test(path))
+                .map(path => {
+                  const dataProp = dataProps.find(p => p.property === path)
+                  return (
+                    <Box
+                      key={path}
+                      direction="row"
+                      align="center"
+                      justify="between"
+                    >
+                      <Text margin={{ horizontal: 'small' }}>
+                        {path}
+                      </Text>
+                      <Box flex={false} direction="row" align="center">
+                        <RadioButton
+                          name="primaryKey"
+                          checked={primaryKey === path}
+                          onChange={() => setPrimaryKey(path)}
+                        />
+                        <Box flex={false} width="small">
+                          {dataProp.options ? (
+                            <Select
+                              multiple
+                              options={dataProp.options}
+                              value={dataProp.values || []}
+                              onChange={(event) => {
+                                const nextDataProps =
+                                  JSON.parse(JSON.stringify(dataProps))
+                                nextDataProps.find(p => p.property === path)
+                                  .values = event.value
+                                setDataProps(nextDataProps)
+                              }}
+                              style={{ width: '100%' }}
+                            />
+                          ) : (
+                            <TextInput
+                              placeholder="search"
+                              value={dataProp.searchText || ''}
+                              onChange={(event) => {
+                                const nextDataProps =
+                                  JSON.parse(JSON.stringify(dataProps))
+                                nextDataProps.find(p => p.property === path)
+                                  .searchText = event.target.value
+                                setDataProps(nextDataProps)
+                              }}
+                              style={{ width: '100%' }}
+                            />
+                          )}
+                        </Box>
+                        <Box flex={false} direction="row">
+                          <Button
+                            icon={<Up />}
+                            hoverIndicator
+                            disabled={!config.indexOf(path)}
+                            onClick={() => {
+                              const index = config.indexOf(path)
+                              const nextConfig = [...config]
+                              nextConfig[index] = nextConfig[index - 1]
+                              nextConfig[index - 1] = path
+                              setConfig(nextConfig)
+                            }}
+                          />
+                          <Button
+                            icon={<Down />}
+                            hoverIndicator
+                            disabled={config.indexOf(path) >= config.length - 1}
+                            onClick={() => {
+                              const index = config.indexOf(path)
+                              const nextConfig = [...config]
+                              nextConfig[index] = nextConfig[index + 1]
+                              nextConfig[index + 1] = path
+                              setConfig(nextConfig)
+                            }}
+                          />
+                          <Button
+                            icon={<Close />}
+                            hoverIndicator
+                            onClick={() => setConfig(config.filter(c => c !== path))}
+                          />
+                        </Box>
+                      </Box>
+                    </Box>
+                  )
+                })}
+
+                <Box margin={{ top: 'small' }} border="top" pad={{ top: 'small' }}>
+                  {dataProps
+                  .filter(p =>
+                    !config.some(path => (path === p.property && !p.dataProps)))
+                  .filter(p => !search || searchExp.test(p.property))
+                  .map(property => (
+                    <Box key={property.property}>
+                      <Box direction="row" align="center" justify="between">
+                        <Text margin={{ horizontal: 'small' }}>
+                          {property.property}
+                        </Text>
+                        <Box flex="shrink" direction="row" align="center" justify="end">
+                          <Box width="small" flex="shrink">
+                            <Text color="light-4" truncate>
+                              {property.example}
+                            </Text>
+                          </Box>
+                          <Button
+                            icon={<Add />}
+                            hoverIndicator
+                            onClick={() => setConfig([...config, property.property])}
+                          />
+                        </Box>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+              </Box>
+            </Box>
           )}
         </Box>
       )}
