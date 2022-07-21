620ae29861ac9696b9e7b08fb18aeab1fcd409c7
Eric Soderberg <eric.soderberg@hpe.com>
2022-07-21 11:38:18 -0700

    Upgraded dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(
+    /Provide the URL of a JSON endpoint and see what it has to offer/i,
+  );
+  expect(linkElement).toBeInTheDocument();
 });
